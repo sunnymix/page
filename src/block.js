5b4bf7671835efe402710becf752d5af91f5624f
sunnymix <sunny@sunnymix.com>
2020-07-29 19:26:07 +0800

    Set padding in content

--- src/block.js
+++ src/block.js
@@ -134,11 +134,13 @@ Block.prototype.loadStyle = function () {
 
         this.ele.css({
             borderBottomWidth: 0,
+            paddingTop: 0,
             paddingBottom: 0
         });
 
         this.contentEle.css({
             borderBottom: style.borderBottom,
+            paddingTop: style.paddingTop,
             paddingBottom: style.paddingBottom
         });
     }
