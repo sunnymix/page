a868d2c07f17230787ee16ee491418931d7ad4bf
sunnymix <sunny@sunnymix.com>
2020-10-14 21:05:25 +0800

    Refactor markdown import parse workflow

--- src/transfer/markdownimport.js
+++ src/transfer/markdownimport.js
@@ -6,14 +6,6 @@
         thiz.check = check || 0;
     }
 
-    BlockDef.prototype.data = function () {
-        return {
-            schema: thiz.schema,
-            text: thiz.text,
-            check: thiz.check
-        };
-    };
-
     function Parser(order, schema, regexp) {
         var thiz = this;
         thiz.order = order || 0;
@@ -96,9 +88,8 @@
             var rowText = rows[i];
 
             var parser = Parser.get(rowText);
-            var blockData = thiz.applyParser(rowText, parser);
 
-            if (isNone(blockData)) {
+            if (isNone(parser)) {
                 continue;
             }
 
@@ -115,12 +106,12 @@
             // ------> switch toggle, cache data ------>
 
             // Code toggle
-            if (blockData.schema == SCHEMA.CODE) {
+            if (parser.schema == SCHEMA.CODE) {
                 isPushData = false; // ``` has no content
                 isHandleCache = false;
                 isStartToggle = !isToggling;
                 isStopToggle = isToggling;
-            } else if (blockData.schema == SCHEMA.GRID) {
+            } else if (parser.schema == SCHEMA.GRID) {
                 isPushData = false;
                 isHandleCache = false;
                 isStartToggle = !isToggling;
@@ -147,15 +138,14 @@
             }
 
             if (isStartToggle) {
-                toggleSchema = blockData.schema;
+                toggleSchema = parser.schema;
                 toggleParser = parser;
             }
 
             if (isPushData) {
                 isToggling = isAllNotNone(toggleSchema, toggleParser);
-                if (isToggling) {
-                    blockData = thiz.applyParser(rowText, toggleParser);
-                }
+                var selectParser = isToggling ? toggleParser : parser;
+                var blockData = thiz.applyParser(rowText, selectParser);
                 if (isNotNone(blockData)) {
                     blockDataArray.push(blockData);
                 }
@@ -187,86 +177,6 @@
         }
 
         return parser.parse.call(thiz, rowText, schemaText, contentText);
-    };
-
-    MarkdownImport.prototype.getParser = function (rowText) {
-        var thiz = this;
-        var thiz = this;
-        if (isBlank(rowText)) {
-            return null;
-        }
-        var parsers = thiz.getParses();
-
-        var regexp = '.*';
-        var parse = thiz.parseText;
-
-        for (var regexpText in parsers) {
-            if (new RegExp(regexpText).test(rowText)) {
-                regexp = regexpText;
-                parse = parsers[regexpText];
-                break;
-            }
-        }
-
-        return {
-            regexp: regexp,
-            parse: parse
-        };
-    };
-
-    MarkdownImport.prototype.getParses = function () {
-        var thiz = this;
-        if (isNotNone(thiz.parses)) {
-            return thiz.parses;
-        }
-        map = {};
-
-        map['^(###)(.*)$'] = thiz.parseH3;
-        map['^(##)(.*)$'] = thiz.parseH2;
-        map['^(#)(.*)$'] = thiz.parseH1;
-        map['^(\\[[.\\sxX√]?\\])(.*)$'] = thiz.parseTask;
-        map['^```\\s*$'] = thiz.parseCode;
-        map['^(\\|)(.)+(\\|)$'] = thiz.parseGrid;
-
-        thiz.parses = map;
-        return thiz.parses;
-    };
-
-    MarkdownImport.prototype.parseText = function (sourceText, schemaText, contentText) {
-        return this.createBlock(SCHEMA.TEXT, contentText);
-    };
-
-    MarkdownImport.prototype.parseH1 = function (sourceText, schemaText, contentText) {
-        return this.createBlock(SCHEMA.H1, contentText);
-    };
-
-    MarkdownImport.prototype.parseH2 = function (sourceText, schemaText, contentText) {
-        return this.createBlock(SCHEMA.H2, contentText);
-    };
-
-    MarkdownImport.prototype.parseH3 = function (sourceText, schemaText, contentText) {
-        return this.createBlock(SCHEMA.H3, contentText);
-    };
-
-    MarkdownImport.prototype.parseTask = function (sourceText, schemaText, contentText) {
-        var check = (new RegExp('^\\[[xX√]+\\]$')).test(schemaText) ? 1 : 0;
-        return this.createBlock(SCHEMA.TASK, contentText, check);
-    };
-
-    MarkdownImport.prototype.parseCode = function (sourceText, schemaText, contentText) {
-        return this.createBlock(SCHEMA.CODE, sourceText);
-    };
-
-    MarkdownImport.prototype.parseGrid = function (sourceText, schemaText, contentText) {
-        return this.createBlock(SCHEMA.GRID, '[[[]]]');
-    };
-
-    MarkdownImport.prototype.createBlock = function (schema, text, check) {
-        return {
-            schema: schema,
-            text: text,
-            check: check || 0
-        };
     };
 
     window.MarkdownImport = MarkdownImport;
