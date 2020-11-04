c07dfc6e786fd963795df5cd6cb772af0f4f0dd6
sunnymix <sunny@sunnymix.com>
2020-11-04 11:31:41 +0800

    Refactor folder

---
+++ src/nav/nav.js
@@ -0,0 +1,12 @@
+(function () {
+    function Nav() {
+        var thiz = this;
+        thiz.init();
+    }
+
+    Nav.prototype.init = function () {
+        var thiz = this;
+    };
+
+    window.Nav = Nav;
+})();
